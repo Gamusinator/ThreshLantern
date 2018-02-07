23039954130b956c7c5170a97ce82f762802a186
Gamusinator <danicano1212@gmail.com>
2018-02-07 15:16:11 +0100

    07/02/2018 - 15:15

--- app/src/main/java/gmustudios/threshlantern/MainActivity.java
+++ app/src/main/java/gmustudios/threshlantern/MainActivity.java
@@ -1,6 +1,8 @@
 package gmustudios.threshlantern;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton boto;
+    CoordinatorLayout principal;
+    boolean sk1,sk2,sk3,sk4,sk5,sk6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +27,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Inicialitzacions
+        boto = findViewById(R.id.imageButton);
+        principal = findViewById(R.id.mainlayout);
+
+
+        //Iniciem amb skin1 (s'ha d'iniciar amb un fons negre
+        //principal.setBackgroundResource(R.drawable.skin1_gradient_selector);
+
+        boto = findViewById(R.id.imageButton);
+        boto.setOnClickListener(this);
+
+        FloatingActionButton share = (FloatingActionButton) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+    @Override
+    public void onClick(View view) {
+        //switch per posar diferents skins
+        if (sk1) {
+            //al encendre la llum posem el gradient
+            principal.setBackgroundResource(R.drawable.skin1_gradient_selector);
+            //encenem el flash
+                    /*codi per encendre flash*/
+        }else if (sk2){
+        }else if (sk3){
+            //al encendre la llum posem el gradient
+            principal.setBackgroundResource(R.drawable.skin3_gradient_selector);
+            //encenem el flash
+                    /*codi per encendre flash*/
+        }else if (sk4){
+        }else if (sk5){
+        }else if (sk6){
+        }
     }
 
     @Override
@@ -43,10 +82,63 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_skin1) {
+            Toast.makeText(this, "Skin1", Toast.LENGTH_SHORT).show();
+            sk1=true;
+            sk2=false;
+            sk3=false;
+            sk4=false;
+            sk5=false;
+            sk6=false;
+            return true;
+        }else if (id == R.id.action_skin2){
+            Toast.makeText(this, "Skin2", Toast.LENGTH_SHORT).show();
+            sk2=true;
+            sk1=false;
+            sk3=false;
+            sk4=false;
+            sk5=false;
+            sk6=false;
+            return true;
+        }else if (id == R.id.action_skin3){
+            Toast.makeText(this, "Skin3", Toast.LENGTH_SHORT).show();
+            sk3=true;
+            sk1=false;
+            sk2=false;
+            sk4=false;
+            sk5=false;
+            sk6=false;
+            return true;
+        }else if (id == R.id.action_skin4){
+            Toast.makeText(this, "Skin4", Toast.LENGTH_SHORT).show();
+            sk4=true;
+            sk1=false;
+            sk2=false;
+            sk3=false;
+            sk5=false;
+            sk6=false;
+            return true;
+        }else if (id == R.id.action_skin5){
+            Toast.makeText(this, "Skin5", Toast.LENGTH_SHORT).show();
+            sk5=true;
+            sk1=false;
+            sk2=false;
+            sk3=false;
+            sk4=false;
+            sk6=false;
+            return true;
+        }else if (id == R.id.action_skin6){
+            Toast.makeText(this, "Skin6", Toast.LENGTH_SHORT).show();
+            sk6=true;
+            sk1=false;
+            sk2=false;
+            sk3=false;
+            sk4=false;
+            sk5=false;
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
